905da2fc29c4e6a2486870e468520fd2146d0a5e
godc <caiyenan99@163.com>
2023-02-17 23:14:26 +0800

    将配置文件上传到nacos中,新增网关配置

---
+++ online-learning-gateway/src/main/java/com/cyn/onlinelearning/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.cyn.onlinelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Godc
+ * @description:
+ * @date 2023/2/17 22:52
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
